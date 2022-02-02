e5bf196a5920b042c9086bcb12718290e4c72523
Raquel <raquelabad@hotmail.com>
2022-02-02 20:32:44 +0100

    Sprint3 Solution

--- js/checkout.js
+++ js/checkout.js
@@ -1,14 +1,84 @@
+
 // Get the input fields
-var password = document.querySelector(".password");
-var phone = document.querySelector('.phone');
-var name = document.querySelector('.name');
+var password = document.querySelector("#password");
+var phone = document.querySelector('#phone');
+var name1 = document.querySelector('#name');
+var lastname=document.querySelector('#lastname');
+var address=document.querySelector('#address');
+var email=document.querySelector('#email');
+var emailtxt=document.querySelector('#email').innerHTML;
 
 // Get the error elements
 var errorPassword = document.getElementById("errorPassword");
 var errorName = document.getElementById('errorName');  
-var errorPhone = document.getElementById('errorPhone');  
+var errorLastName = document.getElementById('errorLastName');  
+var errorPhone = document.getElementById('errorPhone');
+var errorAddress=  document.getElementById('errorAddress');
+var errorEmail= document.getElementById('errorEmail');
+
+var allErrors=document.querySelectorAll(".error");
+var allInputs=document.querySelectorAll(".input");
+
+allErrors.forEach(error=>{
+    error.style.display="none";
+});
+
+
 
 // Exercise 8
 function validate() {
+    allErrors.forEach(error=>{
+        error.style.display="none";
+       
+    });
+    allInputs.forEach(error=>{
+       
+        error.style.borderColor="black";
+    });
+    
     // Validate fields entered by the user: name, phone, password, and email
+
+    if(name1.value=='' || name1.value.length<3 || !/^[a-zA-Z]*$/g.test(name1.value)){
+        name1.style.borderColor="red";
+        errorName.style.display="inline-block";  
+        
+    }
+
+    if(lastname.value=='' || lastname.value.length<3 || !/^[a-zA-Z]*$/g.test(lastname.value)){
+        lastname.style.borderColor="red";
+        errorLastName.style.display="block"; 
+        
+    }
+
+    var atpos = emailtxt.indexOf("@");
+    var dotpos = emailtxt.lastIndexOf(".");
+
+    if(email.value=='' || email.value.length<3 || !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/g.test(email.value)){
+        email.style.borderColor="red";
+        errorEmail.style.display="block"; 
+       
+    }  
+
+    if(password.value=='' || password.value.length<3 || !/^[0-9a-zA-Z]+$/g.test(password.value)){
+        password.style.borderColor="red";
+        errorPassword.style.display="block"; 
+       
+    }
+
+    if(address.value=='' || address.value.length<3){
+       
+        address.style.borderColor="red";
+        errorAddress.style.display="block"; 
+        
+    }
+
+    if(phone.value=='' || phone.value.length<3 || !/^\d+$/g.test(phone.value)){
+        phone.style.borderColor="red";
+        errorPhone.style.display="block"; 
+       
+    }
+
+    
+
+
 }
--- js/shop.js
+++ js/shop.js
@@ -78,52 +78,150 @@ var subtotal = {
 var total = 0;
 
 // Exercise 1
-function buy(id) {
+/*function buy(id) {
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
-}
+    for(let i=0;i<products.length;i++){
+        if(products[i].id==id){
+             // 2. Add found product to the cartList array
+            cartList.push(products[i]);
+        }         
+    }   
+    console.log(cartList);
+}*/
 
 // Exercise 2
 function cleanCart() {
-
+    /*cartList=[];
+    console.log(cartList);*/
+    cart=[];
+    console.log(cart);
 }
 
 // Exercise 3
 function calculateSubtotals() {
-    // 1. Create a for loop on the "cartList" array 
-    // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
-}
+    // 1. Create a for loop on the "cartList" array
+   
+    for(let i=0;i<cart.length;i++) { 
+
+        // 2. Implement inside the loop an if...else or switch...case to add the quantities of each type of product, obtaining the subtotals: subtotalGrocery, subtotalBeauty and subtotalClothes
+        if(cart[i].type=="grocery"){
+            subtotal.grocery.value+=cart[i].price;
+        }else if(cart[i].type=="beauty"){
+            subtotal.beauty.value+=cart[i].price;
+        }else{
+            subtotal.clothes.value+=cart[i].price;
+        }
+    }
+    //console.log(subtotal);
+
+ }
 
 // Exercise 4
 function calculateTotal() {
     // Calculate total price of the cart either using the "cartList" array
+    //console.log(subtotal);
+    for (let x in subtotal)
+    total+=subtotal[x].value;
+    //console.log(total);
 }
 
 // Exercise 5
-function generateCart() {
+/*function generateCart(cartList) {
     // Using the "cartlist" array that contains all the items in the shopping cart, 
     // generate the "cart" array that does not contain repeated items, instead each item of this array "cart" shows the quantity of product.
-}
+    for(let x in cartList){
+        if(cart!=''){
+            for(let y in cart){
+                 if (cart[y].id!=cartList[x].id){
+                    cart.push({id:cartList[x].id,name:cartList[x].name,price:cartList[x].price,type:cartList[x].type,quantity:1,subtotal:cartList[x].price,subtotalWithDiscount:cartList[x].subtotalWithDiscount});
+                   break;
+                }else{                 
+                    cart[y].quantity+=1;
+                    cart[y].subtotal+=cartList[x].price;
+                }
+            }   
+        }else{
+            cart.push({id:cartList[x].id,name:cartList[x].name,price:cartList[x].price,type:cartList[x].type,quantity:1,subtotal:cartList[x].price,subtotalWithDiscount:cartList[x].subtotalWithDiscount});
+        } 
+    }
+    console.log(cart);
+}*/
 
 // Exercise 6
-function applyPromotionsCart() {
+function applyPromotionsCart(cart) {
     // Apply promotions to each item in the array "cart"
+    for(let x in cart){
+        if (cart[x].id==1 && cart[x].quantity>=3){
+            cart[x].subtotalWithDiscount=cart[x].quantity*10;
+        }
+
+        if(cart[x].id==3 && cart[x].quantity>=10){
+            cart[x].subtotalWithDiscount=cart[x].subtotal-(cart[x].subtotal*2/3);
+        }
+    }
+    console.log(cart);
 }
 
 // Exercise 7
 function addToCart(id) {
     // Refactor previous code in order to simplify it 
     // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+    for(let i=0;i<products.length;i++){
+        if(products[i].id==id){
+             // 2. Add found product to the cart array or update its quantity in case it has been added previously.
+            if(cart!=''){
+                  var exists= cart.findIndex(el => el.id==id); 
+
+                     if(exists === -1){
+                         cart.push({id:products[i].id,name:products[i].name,price:products[i].price,type:products[i].type,quantity:1,subtotal:products[i].price,subtotalWithDiscount:0});
+                  
+                    }else if(exists >-1){
+                        cart[exists].quantity+=1;
+                        cart[exists].subtotal+=cart[exists].price;
+                    }
+                   
+            }else{
+                cart.push({id:products[i].id,name:products[i].name,price:products[i].price,type:products[i].type,quantity:1,subtotal:products[i].price,subtotalWithDiscount:0});
+            }                         
+        }         
+    }   
+    console.log(cart);
 }
 
 // Exercise 9
 function removeFromCart(id) {
-    // 1. Loop for to the array products to get the item to add to cart
-    // 2. Add found product to the cartList array
+    // 1. Loop for to the array products to get the item to remove to cart
+     if(cart!=''){
+        var exists= cart.findIndex(el => el.id==id); 
+            if(exists>-1){
+                var index = cart.map(x => {
+                    return x.id;
+                  }).indexOf(id);
+
+                  if(cart[index].quantity>1){
+                        cart[index].quantity-=1;
+                  }else{
+                       cart.splice(index, 1);
+                  }
+                   
+            }else{
+                 alert('El producto no está en el carrito.');
+            }
+                              
+    }else{
+        alert('El carrito está vacío.');
+    }   
+    console.log(cart);
+    // 2. remove found product to the cartList array
 }
 
 // Exercise 10
 function printCart() {
     // Fill the shopping cart modal manipulating the shopping cart dom
+    $('.list').append('<body>');
+    for(let i=0;i<cart.length;i++){
+        $('.list').append('<tr><td>'+cart[i].name+'</td><td>'+cart[i].quantity+'</td><td>'+cart[i].subtotal)+'</td></tr>';
+       
+    }
+    $('.list').append('</body></table>');
 }
